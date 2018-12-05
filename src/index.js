5c2e45212c42882075c76c04957f08fba0ecdd66
René Jensen <rene_spam@sroo.dk>
2018-12-05 21:35:37 +0100

    Add client which exposed a telegram via events

--- src/index.js
+++ src/index.js
@@ -38,3 +38,15 @@ import _ImstReader from './products/reader/Imst';
 export { _ImstReader as ImstReader };
 import _AmberWirelessReader from './products/reader/amber-wireless';
 export { _AmberWirelessReader as AmberWirelessReader };
+
+/**
+* Client
+*/
+import _TelegramClient from './client/telegramClient';
+export { _TelegramClient as TelegramClient };
+
+/**
+* Meters
+*/
+import _KamstrupMultical21Meter from './products/meters/kamstrup-multical-21-meter';
+export { _KamstrupMultical21Meter as KamstrupMultical21Meter };
